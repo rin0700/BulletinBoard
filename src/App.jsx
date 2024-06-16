183568b9fdddef57f31bd6d1d4c286bab394e237
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-16 19:26:50 +0900

    API呼び出し

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'react'
 import './App.css'
+import { getThreads } from './utils/threads.js'
 
 function App() {
+  const initialURL = "https://railway.bulletinboard.techtrain.dev/threads?offset=0";
+
+  useEffect(() => {
+    const fetchThreads = async () => {
+      let res = await getThreads(initialURL);
+      console.log(res);
+    }
+    fetchThreads();
+  },[])
 
   return (
     <>
-      <h1>TechTrain</h1>
+      <h1>TechTrain ReactStation1</h1>
     </>
   )
 }
