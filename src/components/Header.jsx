0079c36269a21f9a3cd6dc8eed03a3f766114c4f
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-30 23:14:06 +0900

    navigate設定

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import "../Style/Header.css";
 function Header() {
+  const navigate = useNavigate();
+  const handleNewThread = () => {
+    navigate('/threads/new');
+  }
+  const handleThread = () => {
+    navigate('/');
+  }
   return (
     </header>
   );
