cb50b817d16bc70030a29af7b89e90e3a7b7c3fe
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-30 21:40:56 +0900

    APIの取得

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,2 +1,8 @@
   const [data, setData] = useState([])
 
+  useEffect(() => {
+    fetch('https://railway.bulletinboard.techtrain.dev/threads?offset=0')
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .catch(() => alert('データの取得に失敗しました'))
+  }, [])
