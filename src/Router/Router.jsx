54ea171049c40b2e1f2af32c04b922975068636b
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-30 23:17:01 +0900

    Router.jsx作成

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NewThread from '../page/newThread'
+import Thread from '../page/Thread'
+
+const Router = () => {
+  return (
+  )
+}
+
+export default Router
