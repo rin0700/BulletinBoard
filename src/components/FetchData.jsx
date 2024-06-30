dd82560634e4d02cac6d3c97b58ea9ce7827e5af
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-30 21:41:24 +0900

    Mapで取得したデータを一覧表示

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -6,3 +6,18 @@
       .then((json) => setData(json))
       .catch(() => alert('データの取得に失敗しました'))
   }, [])
+  console.log(data)
+  return (
+    <div className="fetch-data-container">
+      {(!data || data.length === 0) ? (
+        <div>Loading</div>
+      ) : (
+        <div className="data-list">
+          {data.map((item, index) => (
+            <div key={index} className="data-item">{item.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
