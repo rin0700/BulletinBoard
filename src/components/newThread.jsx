3511911efae26836740cf41c7ddd08a85b9290d8
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-07-03 21:58:57 +0900

    NewThread作成

--- src/components/newThread.jsx
+++ src/components/newThread.jsx
@@ -1,3 +1,11 @@
 import { useState } from 'react'
 import axios from 'axios'
 
+function NewThread() {
+  const [newThread, setNewThread] = useState('')
+
+  return (
+  )
+}
+
+export default NewThread
