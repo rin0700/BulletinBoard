1129350bbecdf11091a72ef40da89cdf5ae4d85c
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-07-03 21:59:25 +0900

    UIの作成

--- src/components/newThread.jsx
+++ src/components/newThread.jsx
@@ -5,6 +5,14 @@ function NewThread() {
   const [newThread, setNewThread] = useState('')
 
   return (
+    <>
+      <h2>スレッド新規作成</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newThread} onChange={e => setNewThread(e.target.value)} required placeholder="スレッド名" />
+        <br /><br />
+        <input type="submit" value="作成" />
+      </form>
+    </>
   )
 }
 
